8f052c346c0cfde362ce65c5c9e18737f7e5ed44
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-02 15:17:27 -0400

    Completed

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let home: Coordinate
+    var isHungry = true
+    
+    init(firstName: String, lastName: String, home: Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+    }
+    
+    mutating func eatPizza(from service: PizzaDeliveryService) -> Bool {
+        
+        var willEatFromHome: Bool
+        if isHungry == true && service.isInRange(to: home) {
+            isHungry = false
+            willEatFromHome = true
+        } else {
+            willEatFromHome = false
+        }
+        
+        if service.isInRange(to: home) == false {
+            willEatFromHome = false
+        }
+        return willEatFromHome
+    }
+    
+}
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    
+    let location: Coordinate
+    var pizzasAvailable = 10
+    
+    init(location: Coordinate) {
+        self.location = location
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        return destination.distance(to: location) <= 5000
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        
+        var isDeliverable: Bool
+        
+        if isInRange(to: destination) {
+            if pizzasAvailable == 0 {
+                isDeliverable = false
+            } else {
+                pizzasAvailable -= 1
+                isDeliverable = true
+            }
+        } else {
+            isDeliverable = false
+        }
+        
+        return isDeliverable
+        
+    }
+    
+    
+    
+    
+}
